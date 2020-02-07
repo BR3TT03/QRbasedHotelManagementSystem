bc00601ffc586aa172939db1e45cd810d6605ca8
BR3TT03 <rockbrett03@gmail.com>
2020-02-07 20:50:38 +0545

    reomte mysql configured

---
+++ bin/menu.php
@@ -0,0 +1,25 @@
+<?php
+	error_reporting("E-NOTICE");
+?>
+<?php
+//	session_start();
+	//if(!$_SESSION['uname'] && (!$_SESSION['pass'])){
+	//	header("location: ../login.php");
+	//}
+?>
+<div id="top">
+			<h1><a href="#">Content Management system</a></h1>
+			<div id="top-navigation">
+				Welcome <a href="#"><strong>Administrator</strong></a>
+				<span>|</span>
+				<a href="index.php">Log out</a>
+			</div>
+		</div>
+<div id="navigation">
+			<ul>
+			    <li><a href="index.php"><span>Menu LIst</span></a></li>
+				<li><a href="orders.php"><span>orders</span></a></li>
+				<li><a href="orders.php"><span>payment</span></a></li>
+	
+			</ul>
+		</div>
